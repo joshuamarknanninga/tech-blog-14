9979cc888091bf82c35cf1ddbfbdd4f8b9d200a5
Joshua Nanninga <jmarknanninga@gmail.com>
2024-09-14 14:17:01 -0500

    updated User.js

--- models/User.js
+++ models/User.js
@@ -31,9 +31,11 @@ User.init(
     password: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+      len: [8],
     },
   },
-  {
+  
     hooks: {
       beforeCreate: async (newUserData) => {
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
