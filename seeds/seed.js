e51c7761ad0aa526376c6675bc5b2429b9dcc8b1
Joshua Nanninga <jmarknanninga@gmail.com>
2024-09-14 14:26:07 -0500

    added console.logs in seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -6,9 +6,13 @@ const sequelize = require('../config/connection');
 
 const seedAll = async() => {
     await sequelize.sync({ force: true });
+    console.log('Database synced!');
     await seedUsers();
+    console.log('Users seeded!');
     await seedPosts();
+    console.log('Posts seeded!');
     await seedComments();
+    console.log('Comments seeded!');
     process.exit(0);
 };
 
