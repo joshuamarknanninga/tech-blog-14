91f293bc87c0f4579e62677c1f52379e9002215c
Joshua Nanninga <jmarknanninga@gmail.com>
2024-09-14 15:47:15 -0500

    updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,14 +3,13 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  'tech_blog',
+  '',
+  '',
   {
-    host: process.env.DB_HOST,
+    host: 'localhost',
     dialect: 'postgres',
-    port: process.env.DB_PORT,
-    logging: false, // Disable logging; default: console.log
+    port: 5432,
   }
 );
 
