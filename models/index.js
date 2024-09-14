b08b75eb29e4577cba3f265d0534c47d4124c556
Joshua Nanninga <jmarknanninga@gmail.com>
2024-09-14 14:06:35 -0500

    updated all code in index.js

--- models/index.js
+++ models/index.js
@@ -1,34 +1,38 @@
+// models/index.js
 const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
+// Associations
+
+// User-Post: One-to-Many
 User.hasMany(Post, {
-    foreignKey: 'user_id'
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
 });
 
 Post.belongsTo(User, {
-    foreignKey: 'user_id',
-    onDelete: "cascade"
+  foreignKey: 'user_id',
+});
+
+// User-Comment: One-to-Many
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
 });
 
 Comment.belongsTo(User, {
-    foreignKey: 'user_id',
-    onDelete: "cascade"
+  foreignKey: 'user_id',
+});
+
+// Post-Comment: One-to-Many
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE',
 });
 
 Comment.belongsTo(Post, {
-    foreignKey: 'post_id',
-    onDelete: "cascade"
+  foreignKey: 'post_id',
 });
-
-User.hasMany(Comment, {
-    foreignKey: 'user_id',
-    onDelete: "cascade"
-});
-
-Post.hasMany(Comment, {
-    foreignKey: 'post_id',
-    onDelete: "cascade"
-})
 
 module.exports = { User, Post, Comment };
