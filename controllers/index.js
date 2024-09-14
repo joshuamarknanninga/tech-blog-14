67ecff1b009243a9df3ad0e5256541277fa3ffba
Joshua Nanninga <jmarknanninga@gmail.com>
2024-09-14 14:10:27 -0500

    updated all code in controllers/index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,12 @@
+// controllers/index.js
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes.js');
-const dashboardRoutes = require('./dashboard-routes.js');
+
 router.use('/api', apiRoutes);
-router.use('/', homeRoutes);
-router.use('/dashboard', dashboardRoutes);
+
+// Fallback route
 router.use((req, res) => {
-    res.status(404).end();
+  res.status(404).end();
 });
+
 module.exports = router;
