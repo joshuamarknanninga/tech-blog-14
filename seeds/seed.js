03e55905106123eac35e3fca9c251c019ef563fd
Joshua Nanninga <jmarknanninga@gmail.com>
2024-09-14 14:27:21 -0500

    added error handling in seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -5,15 +5,24 @@ const seedComments = require('./comment');
 const sequelize = require('../config/connection');
 
 const seedAll = async() => {
-    await sequelize.sync({ force: true });
-    console.log('Database synced!');
-    await seedUsers();
-    console.log('Users seeded!');
-    await seedPosts();
-    console.log('Posts seeded!');
-    await seedComments();
-    console.log('Comments seeded!');
-    process.exit(0);
+    try {
+        await sequelize.sync({ force: true });
+        console.log('Database synced!');
+
+        await seedUsers();
+        console.log('Users seeded!');
+
+        await seedPosts();
+        console.log('Posts seeded!');
+
+        await seedComments();
+        console.log('Comments seeded!');
+
+        process.exit(0);
+    } catch (error) {
+        console.error('Failed to seed database:', error);
+        process.exit(1);
+    }
 };
 
 seedAll();
