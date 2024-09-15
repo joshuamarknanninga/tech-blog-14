1bf2cac54b8a4e0b8c517ace09f3398f0458b189
Joshua Nanninga <jmarknanninga@gmail.com>
2024-09-14 21:21:12 -0500

    updated Post.js

--- models/Post.js
+++ models/Post.js
@@ -4,40 +4,41 @@ const sequelize = require('../config/connection');
 class Post extends Model {}
 
 Post.init(
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      title: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      content: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-        validate: {
-          len: [1],
-      },
-      user_id: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: 'user',
-          key: 'id'
-        }
-      }
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    
-      sequelize,
-      timestamps: true,
-      createdAt: 'created_at',
-      updatedAt: false,
-      freezeTableName: true,
-      underscored: true,
-      modelName: 'post',
-    }
-  );
-  
-  module.exports = Post;
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [1], // Ensures content is at least 1 character long
+      },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at', // Changed to true to track updates
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post',
+  }
+);
+
+module.exports = Post;
