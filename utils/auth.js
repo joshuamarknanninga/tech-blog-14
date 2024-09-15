e9627dec2159490a9bd132e207bbef44ce616a7b
Joshua Nanninga <jmarknanninga@gmail.com>
2024-09-14 21:19:45 -0500

    updated middleware

--- utils/auth.js
+++ utils/auth.js
@@ -1,7 +1,13 @@
 // utils/auth.js
 const withAuth = (req, res, next) => {
   if (!req.session.loggedIn) {
-    res.redirect('/login');
+    if (req.headers.accept && req.headers.accept.includes('application/json')) {
+      // Respond with a 401 status code for API requests
+      res.status(401).json({ message: 'You must be logged in to access this resource.' });
+    } else {
+      // Redirect for non-API requests
+      res.redirect('/login');
+    }
   } else {
     next();
   }
