b0114d6fa9f8c3faba3add0bac4f07a9fe24229f
Joshua Nanninga <jmarknanninga@gmail.com>
2024-09-14 21:22:00 -0500

    updated User.js

--- models/User.js
+++ models/User.js
@@ -21,33 +21,46 @@ User.init(
       allowNull: false,
       unique: true,
       validate: {
-        len: [4, 20],
-      }
+        len: [4, 20], // Username length validation
+      },
     },
     email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-          isEmail: true,
-        },
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true, // Checks if the value is a valid email
+      },
     },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-      len: [8],
+        len: [8, 128], // Password length validation (8 to 128 characters)
+      },
     },
   },
-  
+  {
     hooks: {
       beforeCreate: async (newUserData) => {
-        newUserData.password = await bcrypt.hash(newUserData.password, 10);
-        return newUserData;
+        try {
+          newUserData.password = await bcrypt.hash(newUserData.password, 10);
+          return newUserData;
+        } catch (err) {
+          console.error('Error hashing password during creation:', err);
+          throw new Error('Failed to hash password');
+        }
       },
       beforeUpdate: async (updatedUserData) => {
-        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-        return updatedUserData;
+        try {
+          if (updatedUserData.password) {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+          }
+          return updatedUserData;
+        } catch (err) {
+          console.error('Error hashing password during update:', err);
+          throw new Error('Failed to hash password');
+        }
       },
     },
     sequelize,
