f94da5c95c92d5ca9fdc75169510cfdb09c3f83f
Joshua Nanninga <jmarknanninga@gmail.com>
2024-09-14 21:20:10 -0500

    updated helpers

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,24 +1,20 @@
 // utils/helpers.js
 module.exports = {
   format_date: (date) => {
-    if (!date) return '';
+    if (!date || isNaN(new Date(date))) return '';
     return new Date(date).toLocaleDateString('en-GB'); // Formats to DD/MM/YYYY
   },
-  
-  // Optional: Add more helpers as needed
+
   capitalize: (str) => {
-    if (typeof str !== 'string') return '';
+    if (typeof str !== 'string' || str.length === 0) return '';
     return str.charAt(0).toUpperCase() + str.slice(1);
   },
 
   truncate: (str, len) => {
-    if (str.length > len && str.length > 0) {
-      let new_str = str + ' ';
-      new_str = str.substr(0, len);
-      new_str = str.substr(0, new_str.lastIndexOf(' '));
-      new_str = new_str.length > 0 ? new_str : str.substr(0, len);
-      return new_str + '...';
+    if (typeof str !== 'string' || str.length === 0) return '';
+    if (str.length > len) {
+      return str.substr(0, str.lastIndexOf(' ', len)) + '...';
     }
     return str;
-  }
+  },
 };
